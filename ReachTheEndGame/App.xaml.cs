0ef2e34dbaf6768b829c155f2e1c10334b155519
Bende Huba <bende.huba@studenst.jedlik.eu>
2025-01-29 10:01:16 +0100

    c# project létrehozva #3

---
+++ ReachTheEndGame/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ReachTheEndGame
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

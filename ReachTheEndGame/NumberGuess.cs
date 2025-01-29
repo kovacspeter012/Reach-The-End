feaea0354a8cd0579da98e775ff211878172db1e
Kovács Péter <kovacs.peter@students.jedlik.eu>
2025-01-29 11:33:54 +0100

    játék kész #7

---
+++ ReachTheEndGame/NumberGuess.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReachTheEndGame
+{
+    internal class NumberGuess
+    {
+        public int TheNumber { get; set; }
+        public int NumberOfGuesses { get; set; }
+
+        public NumberGuess(int rndNum)
+        {
+            TheNumber = rndNum;
+            NumberOfGuesses = 0;
+        }
+
+        public string NumGuess(int num)
+        {
+            NumberOfGuesses += 1;
+            if (num < TheNumber)
+            {
+                return ">";
+            }
+            else if (num > TheNumber)
+            {
+                return "<";
+            }
+            else
+            {
+                if (WinOrLose())
+                    return "win";
+                else
+                    return "lose";
+            }
+        }
+
+        private bool WinOrLose()
+        {
+            if (NumberOfGuesses <= 6)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ReachTheEndGame/TestWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ReachTheEndGame
+{
+    /// <summary>
+    /// Interaction logic for TestWindow.xaml
+    /// </summary>
+    public partial class TestWindow : Window
+    {
+        NumberGuess GuessedNumber = new NumberGuess(new Random().Next(1, 101));
+        public TestWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnGuess_Click(object sender, RoutedEventArgs e)
+        {
+
+            string feedback = GuessedNumber.NumGuess(int.Parse(tbGuess.Text));
+            tbGuess.Text = null;
+
+            if (feedback == ">")
+            {
+                lblGuess.Content = "A szám nagyobb!";
+            }
+            else if (feedback == "<")
+            {
+                lblGuess.Content = "A szám kisebb!";
+            }
+            else if (feedback == "win")
+            {
+                lblGuess.Content = "Nyertél!";
+            }
+            else if (feedback == "lose")
+            {
+                lblGuess.Content = "Kitaláltad a számot de sajnos túl sok lépésből!";
+            }
+        }
+
+        private void tbGuess_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex _regex = new Regex("[^0-9]+");
+            e.Handled = Regex.IsMatch(e.Text);
+        }
+
+    }
+}

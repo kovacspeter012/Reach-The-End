7c68f452cbfaacb9d4de984c916e15dd5c4a1846
Bende Huba <bende.huba@studenst.jedlik.eu>
2025-01-29 11:33:26 +0100

    aknakereső elkezdése #1

---
+++ ReachTheEndGame/MineSweeper.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ReachTheEndGame
+{
+    /// <summary>
+    /// Interaction logic for MineSweeper.xaml
+    /// </summary>
+    public partial class MineSweeper : Window
+    {
+        List<MineGameGrid> Bombs { get; set; }
+        bool IsGameStarted { get; set; } = false;
+        MineGameGrid[] MineGameGrids;
+        public MineSweeper()
+        {
+            Bombs = new();
+            MineGameGrids = new MineGameGrid[64];
+
+            InitializeComponent();
+            Loaded += MineSweeper_Loaded;
+        }
+
+        private void MineSweeper_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                grdGame.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1,GridUnitType.Star)});
+                grdGame.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                for(int j = 0; j < 8; j++)
+                {
+                    Label l = new Label() { Content = "", Style= this.FindResource("GameGridStyle") as Style};
+                    grdGame.Children.Add(l);
+                    Grid.SetRow(l, i);
+                    Grid.SetColumn(l, j);
+
+                    MineGameGrids[i*8+j] = new MineGameGrid(l,i*8+j);
+
+                    l.PreviewMouseDown += lblClick;
+                }
+            }
+        }
+
+        private void lblClick(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is not Label) return;
+            Label label = (Label)sender;
+
+            MineGameGrid? mineGameGrid = MineGameGrids.FirstOrDefault(e=>e.Label == label);
+            if (mineGameGrid is null) return;
+
+            if (IsGameStarted)
+            {
+                
+            }
+            else
+            {
+                MineGameLogic.GenerateBombs(MineGameGrids,Bombs,mineGameGrid.Index);
+            }
+        }
+
+    }
+}
---
+++ ReachTheEndGame/MineSweeperLogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReachTheEndGame
+{
+    class MineGameGrid
+    {
+        public Label Label { get; private set; }
+        public int Index { get; private set; }
+        public bool IsBomb { get; set; } = false;
+        public bool IsRevealed { get; set; } = false;
+
+        public MineGameGrid(Label label, int index)
+        {
+            Label = label;
+            Index = index;
+        }
+    }
+
+    static class MineGameLogic
+    {
+        public static void GenerateBombs(MineGameGrid[] mineGameGrids, List<MineGameGrid> bombs, int clickIndex)
+        {
+            //TODO
+        }
+    }
+}
